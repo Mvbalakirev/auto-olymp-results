b99f2a3c2a225807a7e4f6de48d05bbc0fb1b8bb
Max Balakirev <mvbalakirev@yandex.ru>
2024-12-23 23:34:11 +0300

    index page added

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+import olymps.views
 
 urlpatterns = [
     path('students/', include('students.urls')),
     path('olymps/', include('olymps.urls')),
     path('admin/', admin.site.urls),
+    path('', olymps.views.index)
 ]
